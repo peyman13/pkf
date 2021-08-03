175b5f54e72861d8188f834795521578a7da997c
peyman khodabandeh <peyman.developer13@gmail.com>
2021-08-03 11:58:29 +0430

    test

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     {
         return view('user::index');
     }
-
+ 
     /**
      * Show the form for creating a new resource.
      * @return Renderable
