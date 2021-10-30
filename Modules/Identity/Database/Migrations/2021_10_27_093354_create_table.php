e4e259fa5044079d447f2600302746b148694da4
peyman khodabandeh <peyman.developer13@gmail.com>
2021-10-30 08:59:10 +0330

    add identity

---
+++ Modules/Identity/Database/Migrations/2021_10_27_093354_create_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identity', function (Blueprint $table) {
+            
+            $table->id();
+            $table->string('identity', 20);
+            $table->char('mobile', 11);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('');
+    }
+}
