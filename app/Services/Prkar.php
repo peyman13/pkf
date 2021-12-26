1eecf632f2483adff3028ff816a301a456fce725
peyman <peyman.developer13@gmail.com>
2021-12-26 16:40:47 +0330

    config prkar

--- app/Services/Prkar.php
+++ app/Services/Prkar.php
@@ -14,7 +14,7 @@ class Prkar  implements PrkarServiceInterface
 
     private static function _init()
     {
-      self::$_config = config('otp.env.local');
+      self::$_config = config('prkar.env.local');
     }
 
     public function getProvince()
@@ -23,34 +23,25 @@ class Prkar  implements PrkarServiceInterface
 
         try {
             
-            self::$_response = Http::withHeaders(self::$_config['headers'])
-                // ->withOptions(['debug' => true])
+            self::$_response = Http::withHeaders([])
+                ->withOptions(['debug' => true])
                 ->timeout(self::$_config['timeout'])
-                ->get('http://192.168.22.137/prkar/v1/province');
-            
+                ->get(self::$_config['api_url']."/v1/province");
 
-            $response = self::$_response->json();
-
-            if (
-                $response['status'] === true &&
-                isset($response['data']) &&
-                isset($response['data']['sms_status_code']) &&
-                $response['data']['sms_status_code'] == 20
-
-            ) {
+            if (self::$_response->status() == 200) {
                 Log::channel('service_success')
-                    ->info('OTP_Service=>getOTP result=' . json_encode($response, JSON_UNESCAPED_UNICODE));
+                    ->info('Prkar_Service=>getProvince result=' . json_encode(self::$_response, JSON_UNESCAPED_UNICODE));
 
-                return $response['data']['sms_otp_text'];
+                return self::$_response;
             } else {
                 Log::channel('service_faild')
-                    ->emergency('OTP_Service=>getOTP result=' . json_encode($response, JSON_UNESCAPED_UNICODE));
+                    ->emergency('Prkar_Service=>getProvince result=' . json_encode(self::$_response, JSON_UNESCAPED_UNICODE));
 
                 return false;
             }
         } catch (\Exception $e) {
             Log::channel('service_faild')
-                ->emergency('OTP_Service=>getOTP' . $e->getMessage());
+                ->emergency('Prkar_Service=>getProvince' . $e->getMessage());
             return false;
         }
     }
