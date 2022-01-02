25b8281437d195a9be66dc4925135af300539e4b
peyman khodabande <peyman.developer13@gmail.com>
2022-01-02 13:52:53 +0330

    prkar module

---
+++ app/Models/Service.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+
+    protected $fillable = [
+        'users_id',
+        'service_id',
+        'steps',
+        'request',
+        'response',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
