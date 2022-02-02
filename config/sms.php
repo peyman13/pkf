03a006fff7030713a2d4b7c23853bcceeba63a8e
peyman khodabandeh <peyman.developer13@gmail.com>
2022-02-02 09:37:55 +0330

    Update sms.php

--- config/sms.php
+++ config/sms.php
@@ -5,10 +5,10 @@ return [
         'local' => [
             'api_url' => 'http://172.30.30.76/microsms/sms/send2',
             'headers' => [
-                'Api-Key'=>'d7043be08dfe221b56e607712183f786'
+                'Api-Key'=>''
             ],
-            'email' => 'mypishkhan@epishkhan.ir',
-            'password' => 'JHssX9TsEDeFV5ut',
+            'email' => '@.ir',
+            'password' => '',
             'debug' => true,
             'timeout' => 30,
         ],
