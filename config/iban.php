c261323c85be03e858ddcfc4a05db478988ee444
peyman khodabandeh <peyman.developer13@gmail.com>
2022-02-02 09:38:20 +0330

    Update iban.php

--- config/iban.php
+++ config/iban.php
@@ -5,12 +5,12 @@ return [
         'local' => [
             'api_url' => env('IBAN_URL', true),
             'headers' => [
-                "X-API-KEY" => "icsit_prkar@cZCTa3RcGzZ3pAfRK7rnRsV5zMeK", 
-                "Authorization" => 'Basic aWNzaXRfcHJrYXI6alF5QzZ6Rk5WS3lVNjh1QQ=='
+                "X-API-KEY" => "@", 
+                "Authorization" => 'Basic =='
             ],
             "auth" => [
-                "domain" => "icsit_prkar",
-                "pass" => "fac7Ptja3dtYUFWa",
+                "domain" => "",
+                "pass" => "",
                 "service_id" => 100100
             ],
             'debug' => true,
